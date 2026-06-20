e4d8aa044794246ae73d76489a310f8a5edf5505
nngn12 <analeahbasco1@gmail.com>
2026-06-20 17:34:43 +0800

    Update layout with custom earthy palette and design icons

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Mobile menu toggle logic
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-// Toggle mobile menu when hamburger is clicked
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
     hamburger.classList.toggle("active");
 });
 
-// Close mobile menu when a link is clicked
 links.forEach(link => {
     link.addEventListener("click", () => {
         navLinks.classList.remove("active");
